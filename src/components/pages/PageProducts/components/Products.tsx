9ea178bd7032e13e6ac5f685e36ff059870039e5
Artem_Blazhko <artem_blazhko@epam.com>
2021-04-21 23:32:30 +0300

    Add some changes =)

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -11,7 +11,6 @@ import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
 import axios from 'axios';
 import API_PATHS from "constants/apiPaths";
-// import productList from "./productList.json";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -38,19 +37,19 @@ export default function Products() {
   useEffect(() => {
     axios.get(`${API_PATHS.products}/products`)
       .then(res => {
-        console.log("RES: ", res);
-        setProducts(res.data);
+        console.log("RES: ", res.data.products);
+        setProducts(res.data.products);
       });
   }, []);
 
   return (
     <Grid container spacing={4}>
-      {products.map((product: Product) => (
+      {products.map((product: Product, index: number) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={product.image}
+              image={`https://source.unsplash.com/random?sig=${index}`}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
